61687f14cedf81222d0412506a6f0fb85a08d63b
MiguelBalam <191k0019@itscarrillopuerto.edu.mx>
2023-08-24 15:30:36 -0500

    Cambios en prueva ya funciona algo

--- BD/prueva/consultaDatos.php
+++ BD/prueva/consultaDatos.php
@@ -11,22 +11,46 @@ if ($con->connect_error) {
     die("Conexión fallida: " . $con->connect_error);
 }
 
-if (isset($_POST['codigo'])) {
-    $codigo = $_POST['codigo'];
-
-    $sqlEncuesta = "SELECT * FROM respuestas_encuesta WHERE codigo = '$codigo'";
-    $queryEncuesta = mysqli_query($con, $sqlEncuesta);
-
-    if ($queryEncuesta) {
-        if (mysqli_num_rows($queryEncuesta) > 0) {
-            // El código existe, mostrar mensaje de éxito
-            echo "El código existe y tiene respuestas en la base de datos.";
-        } else {
-            // El código no existe, mostrar mensaje de error
-            echo "El código no existe en la base de datos.";
-        }
-    } else {
-        echo "Error al consultar la encuesta.";
-    }
-}
+// Consultar los resultados desde la vista
+$sqlResultados = "SELECT * FROM vista_resultados";
+$queryResultados = mysqli_query($con, $sqlResultados);
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultados de Encuesta</title>
+    <link rel="stylesheet" href="../../BD/prueva/estilostabla.css">
+</head>
+<body>
+    <h1>Resultados de Encuesta</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Pregunta</th>
+                <th>Respuesta</th>
+                <th>Tipo de Respuesta</th>
+                <th>Código</th>
+                <th>Observación</th>
+                <th>Fecha de Creación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row = mysqli_fetch_assoc($queryResultados)) {
+                echo "<tr>";
+                echo "<td>{$row['pregunta']}</td>";
+                echo "<td>{$row['respuesta']}</td>";
+                echo "<td>{$row['tipo_respuesta']}</td>";
+                echo "<td>{$row['codigo']}</td>";
+                echo "<td>{$row['observacion']}</td>";
+                echo "<td>{$row['fecha_creacion']}</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
