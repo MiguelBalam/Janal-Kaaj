0ce199e6f91b912a9ab6305545123c2916962db4
adrianmunoz1624 <51468202+adrianmunoz1624@users.noreply.github.com>
2023-12-01 18:50:04 -0500

    taller viernes

--- BD/prueva/conexion.php
+++ BD/prueva/conexion.php
@@ -1,17 +1,19 @@
 <?php
-
+function conexion(){
 $username  = "janalkaa_admin";
 $password = "janalkaaj2023";
 $servername= "162.241.60.169";
 $dbname = "janalkaa_kaaj";
 
 // Creamos la conexión
-$conexion = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($con,"utf8");
-
-// Verificamos la conexión
-if ($con->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-} else {
-	// echo "Conexión exitosa";
+$conexion = new mysqli($servername, $username, $password, $dbname);
+$acentos = $conexion->query("SET NAMES 'utf8'");
+if($conexion->connect_error){
+    die("Error en la conexión : ". $conexion->connect_errno.
+                                    "-".$conexion->connect_error);
 }
+return $conexion;
+}
+
+?>
+
